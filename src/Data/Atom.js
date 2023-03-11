c6c46430da450216299949858873e228da2e6333
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2023-03-11 19:59:47 +0200

    edit api

--- src/Data/Atom.js
+++ src/Data/Atom.js
@@ -4,7 +4,7 @@ const APICity = atom({
   key: "textState1", // unique ID (with respect to other atoms/selectors)
   // Promasse City => weather
   default:
-    "http://api.openweathermap.org/data/2.5/weather?units=metric&appid=deb1bc65cd1afa8674fe12755514a180&q=", // default value (aka initial value)
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=deb1bc65cd1afa8674fe12755514a180&q=", // default value (aka initial value)
 });
 const APIPoint = atom({
   key: "textState2", // unique ID (with respect to other atoms/selectors)
@@ -16,7 +16,7 @@ const APIForecast = atom({
   key: "textState3", // unique ID (with respect to other atoms/selectors)
   // Promasse City => forecast
   default:
-    "http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=deb1bc65cd1afa8674fe12755514a180&q=", // default value (aka initial value)
+    "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=deb1bc65cd1afa8674fe12755514a180&q=", // default value (aka initial value)
 });
 
 const CitysEG = atom({
